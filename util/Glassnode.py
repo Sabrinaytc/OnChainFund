3404518e13639d9a713e4f6ab31b912fff5b3820
sabrinachiang <sabrinajj22688@gmail.com>
2022-04-15 09:39:07 +0800

    Update Glassnode.py

--- util/Glassnode.py
+++ util/Glassnode.py
@@ -9,7 +9,25 @@ API_KEY = config.Glassnode_API_KEY
 HashRate = config.HashRate
 HashRibbon = config.HashRibbon
 
+## Parameters
+'''
+a : symbol, BTC, ETH
+s : integer (unix timestamp) since
+u : integer (unix timestamp) until
+i : string (frequency interval) 1h, 24h, 10m, 1month, 1w
+f : string (format) JSON, CSV
+'''
+
 res = requests.get(HashRate, params={'a': 'BTC', 'api_key': API_KEY})
 data = res.json() 
 
-print(data)
+df_nested_list = pd.json_normalize(data)
+df_nested_list['t'] = pd.to_datetime(df_nested_list['t'], unit= 's') 
+
+print(df_nested_list)
+
+
+
+def GetData(url):
+
+    return
